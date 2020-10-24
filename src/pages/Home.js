4834a3502e24cbdcc6ce9abcf9b747feae932ad0
shrey <shreysgupta@gmail.com>
2020-10-24 15:59:35 -0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,8 +4,7 @@ import React, { Component, useEffect }  from 'react';
 
 function Home() {
   let clientId = "VWaT69z9TrarRZlBKtRGTg";
-  let clientSecret = "IFzc4TKbHX5A2z3ntfGC7Nra7u69u5B2";
-  let redirectUri = "https://sharp-tesla-464fe0.netlify.app";
+  let redirectUri = "https://sharp-tesla-464fe0.netlify.app/auth";
   let hrefTo = `https://zoom.us/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
   return (
     <div className="App">
