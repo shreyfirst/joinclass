723e70e76c17681dd6db26dd5a62ca990387635a
shrey <shreysgupta@gmail.com>
2020-10-25 09:25:05 -0700

    asdf

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,13 +1,22 @@
 import React, { Component, useEffect }  from 'react';
 import queryString from "query-string";
 import axios from "axios";
+import { useCookies } from "react-cookie";
+
 
 export default function Auth() {
+  const [cookies, setCookie] = useCookies();
+
   useEffect(() => {
     const parsed = queryString.parse(window.location.search);
-    sessionStorage.setItem("token", parsed.token);
-    sessionStorage.setItem("refresh", parsed.refresh);
-    //TODO: change page to dashboard
+    setCookie("token", parsed.token, {
+      path: "/"
+    });
+    setCookie("refresh", parsed.refresh, {
+      path: "/"
+    });
+    //dashboard page
+    window.location.assign("/dashboard")
   }, []);
 
   return (
